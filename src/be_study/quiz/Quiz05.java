9b3af1aab6d9d6260578ec434fd4ec6f94620dde
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-05-08 16:24:49 +0900

    퀴즈5 연산자, 변수입력

---
+++ src/be_study/quiz/Quiz05.java
@@ -0,0 +1,28 @@
+package be_study.quiz;
+
+import java.util.Scanner;
+
+public class Quiz05 {
+
+	public static void main(String[] args) {
+		//		1.
+		int num = 10;
+		System.out.println(num != 0 ? (num > 0 ? "양수" : "음수") : "0");
+
+		//		2.
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("첫번째 숫자를 입력하세요 : ");
+		int num1 = scanner.nextInt();
+		System.out.printf("입력한 숫자=%d\n", num1);
+		System.out.print("두번째 숫자를 입력하세요 : ");
+		int num2 = scanner.nextInt();
+		System.out.printf("입력한 숫자=%d\n\n", num2);
+
+		System.out.printf("더하기 : %d+%d=%d\n", num1, num2, num1 + num2);
+		System.out.printf("빼기 : %d-%d=%d\n", num1, num2, num1 - num2);
+		System.out.printf("곱하기 : %d*%d=%d\n", num1, num2, num1 * num2);
+		System.out.printf("나누기 : %d/%d=%f\n", num1, num2, (double) num1 / num2);
+	}
+
+}
