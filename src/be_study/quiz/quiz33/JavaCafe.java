04a46241a6948c67d3a47ad9b160411f8fdcbf2c
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-05-26 10:27:16 +0900

    퀴즈33 클래스 메소드

---
+++ src/be_study/quiz/quiz33/JavaCafe.java
@@ -0,0 +1,28 @@
+package be_study.quiz.quiz33;
+
+public class JavaCafe {
+	public static void main(String[] args) {
+		printPay(10.00, 40);
+		printPay(10.00, 50);
+		printPay(7.50, 38);
+		printPay(8.50, 66);
+	}
+
+	// 시급과 일한 시간을 입력받아, 주급을 출력
+	public static void printPay(double basePay, int hours) {
+		if (basePay < 8) {
+			System.out.println("최저 시급 에러!");
+		} else if (hours > 60) {
+			System.out.println("초과 근무시간 에러");
+		} else {
+			double pay = 0.0;
+			if (hours <= 40) {
+				pay = hours * basePay;
+			} else {
+				pay = 40 * basePay + (hours - 40) * basePay * 1.5;
+			}
+			/* 해당 메소드를 완성하세요. */
+			System.out.printf("$ %.2f\n", pay);
+		}
+	}
+}
