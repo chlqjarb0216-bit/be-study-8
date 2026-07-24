9931b878529efb48949af03f4048cc6830e89edd
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-07-24 11:21:28 +0900

    DB 연동 insert, delete 처리

--- src/be_study/db/v2/DBSampleMain.java
+++ src/be_study/db/v2/DBSampleMain.java
@@ -1,5 +1,6 @@
 package be_study.db.v2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBSampleMain {
@@ -8,6 +9,61 @@ public class DBSampleMain {
 
 		//DB dept 테이블 관련 데이터 접근 담당 객체
 		DeptDAO deptDAO = new DeptDAO();
+
+		//insert 저장
+
+		int result1 = deptDAO.saveDept(90, "DEV", "SEOUL");
+		if (result1 > 0) {
+			System.out.println("저장 성공 ~ 111");
+		}
+
+		Dept saveDept1 = new Dept();
+		saveDept1.setDeptno(91);
+		saveDept1.setDname("Adv");
+		saveDept1.setLoc("Busan");
+
+		int result2 = deptDAO.saveDept(saveDept1);
+		if (result2 > 0) {
+			System.out.println("저장 성공 ~ 222");
+		}
+		//-----------------------------------------------------------
+
+		int result3 = deptDAO.removeDept(90);
+		int result4 = deptDAO.removeDept(saveDept1);
+
+		if (result3 > 0) {
+			System.out.println("90번 부서 삭제됨");
+		}
+		if (result4 > 0) {
+			System.out.println("91번 부서 삭제됨");
+		}
+
+		//80번 부서 삭제
+		deptDAO.removeDept(80);
+		Dept rmd1 = new Dept(80, null, null);
+		deptDAO.removeDept(rmd1);
+
+		//-----------------------------------------------------------
+
+		//여러개를 저장 하는 케이스
+
+		List<Dept> saveDeptList = new ArrayList<Dept>();
+		saveDeptList.add(new Dept(81, "dn81", "loc81"));
+		saveDeptList.add(new Dept(82, "dn82", "loc82"));
+		saveDeptList.add(new Dept(83, "dn83", "loc83"));
+
+		int result5 = 0;
+		for (Dept dept : saveDeptList) {
+			int r = deptDAO.saveDept(dept);
+			// r : 1	0
+			result5 += r;
+			if (r > 0) {
+				System.out.println("dept 저장 성공");
+			}
+		}
+		System.out.println("반복문 통해서 총 몇개가 저장되었나? 행의 수 합 : " + result5);
+
+		//-----------------------------------------------------------
 
 		//40번 부서의 이름 확인! ???? 정보가 어디에???
 		Dept dept1 = deptDAO.findDeptByDeptno(30);
