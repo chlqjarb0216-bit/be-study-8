b7e8b631edd2f199743ee2409ca2e51eeb17c24a
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-07-29 13:47:36 +0900

    퀴즈 59 JSON 파싱, DTO 구조 설계

---
+++ src/be_study/quiz/quiz59/Item.java
@@ -0,0 +1,37 @@
+package be_study.quiz.quiz59;
+
+public class Item {
+	int id;
+	String name;
+	int quantity;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", quantity=" + quantity + "]";
+	}
+
+}
